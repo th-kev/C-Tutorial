ba606ffeb7c321fd890c3a8f25a9ce71a5a65244
th-kev <theiss.kevin2003@gmail.com>
2023-05-03 17:47:22 +0200

    02.05

--- Chapter07_Arrays/ContentChapter7/Lib.c
+++ Chapter07_Arrays/ContentChapter7/Lib.c
@@ -13,3 +13,31 @@ float meanArray(int *array, unsigned int length)
 
     return (float)(sum) / (float)(length);
 }
+
+int minArray(int *array, unsigned int length)
+{
+
+    int current_min = array[0];
+
+    for(unsigned int i = 0; i < length; i++)
+    {
+        if (array[i] < current_min)
+        {
+            current_min = array[i];
+        }
+    }
+    return current_min;
+}
+
+int maxArray(int *array, unsigned int length)
+{
+    int current_max = array[0];
+    for(unsigned int i = 0; i < length; i++)
+    {
+        if (array[i] > current_max)
+        {
+            current_max = array[i];
+        }
+    }
+    return current_max;
+}
--- Chapter07_Arrays/ContentChapter7/Lib.h
+++ Chapter07_Arrays/ContentChapter7/Lib.h
@@ -3,4 +3,8 @@
 
 float meanArray(int *array, unsigned int length);
 
+int minArray(int *array, unsigned int length);
+
+int maxArray(int *array, unsigned int length);
+
 #endif
