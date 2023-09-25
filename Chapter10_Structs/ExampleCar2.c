8d8044c590471b065f3e4d289be34f5fcc6c3687
Kevin <119873815+th-kev@users.noreply.github.com>
2023-09-25 23:56:08 +0200

    25.09.2023

---
+++ Chapter10_Structs/ExampleCar2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct Date
+{
+    unsigned int year;
+    unsigned int month;
+    unsigned int day;
+}Date_t;
+
+
+typedef struct LicensePlate
+{
+    char region_code[3];
+    char alpha_code[2];
+    char numeric_code[4];
+    bool is_histotic;
+    bool is_electro;
+    Date_t tuv_until_date;
+}LicensePlate_t;
+
+typedef struct CarBase
+{
+    char brand[50];
+    char model[50];
+    uint16_t year;
+    uint16_t hp;
+} CarBase_t;
+
+typedef struct CarStats
+{
+    CarBase_t base;
+    LicensePlate_t plate;
+} CarStats_t;
+
+
+int main()
+{
+    const CarStats_t mercedes = {
+        .base = {.brand = "Mercedes", .model = "E-Klasse", .year = 2022, .hp = 400},
+        .plate = {  .region_code = "WN",
+                    .alpha_code = "T",
+                    .numeric_code = "2003",
+                    .is_electro = false,
+                    .is_histotic = false}
+    };
+
+
+    return 0;
+}
