cb2ef8628470ad5f7487151da355057e873058d1
Kevin <119873815+th-kev@users.noreply.github.com>
2023-10-08 20:51:11 +0200

    08.10.2023

--- Chapter12_Advanced1/ContentChapter12/Main.c
+++ Chapter12_Advanced1/ContentChapter12/Main.c
@@ -17,4 +17,38 @@ int comp(const void *elem1, const void *elem2)
 
 int main(int argc, char *argv[])
 {
+    if (argc == 1)
+    {
+        printf("You have to eunter 3 args!\n");
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
+        printf("File %s not found!\n", argv[1]);
+        return 1;
+    }
+
+    int length = atoi(argv[2]);
+    int *data = (int *)malloc(length * sizeof(int));
+
+    for (unsigned int i = 0; i < length; i++)
+    {
+        fscanf(fp, "%d", &data[i]);
+        printf("%d\n", data[i]);
+    }
+
+    fclose(fp);
+    printf("\n");
+    fp = fopen(argv[1], "w");
+
+    qsort(data, length, sizeof(int), comp);
+
+    for (unsigned int i = 0; i < length; i++)
+    {
+        fprintf(fp, "%d\n", data[i]);
+        printf("%d\n", data[i]);
+    }
+
 }
