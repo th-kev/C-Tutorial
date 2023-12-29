eea393f11a57ff04920dedef857e87e04f74a393
th-kev <theiss.kevin2003@gmail.com>
2023-12-29 12:44:58 +0100

    29.12.2023

--- Chapter16_SimpleCSVReader/Record.c
+++ Chapter16_SimpleCSVReader/Record.c
@@ -3,3 +3,37 @@
 #include <stdlib.h>
 
 #include "Record.h"
+
+
+value_pair_t *create_pairs(const size_t num_pairs)
+{
+    value_pair_t *values = (value_pair_t*)malloc(num_pairs * sizeof(value_pair_t));
+    return values;
+}
+
+void free_pairs(value_pair_t *pairs, const size_t num_pairs)
+{
+    if (NULL == pairs)
+    {
+        return;
+    }
+    free(pairs);
+
+}
+
+records_t *create_records()
+{
+    records_t *records = (records_t*)malloc(sizeof(records_t));
+    return records;
+}
+
+void free_records(records_t *records)
+{
+    if (NULL == records)
+    {
+        /* code */
+        return;
+    }
+    free(records->values);
+    free(records);
+}
--- Chapter16_SimpleCSVReader/Record.h
+++ Chapter16_SimpleCSVReader/Record.h
@@ -7,11 +7,32 @@
 
 /* DEFINES */
 
+#define NUM_VALUES 2
 
 /* TYPES */
+typedef struct
+{
+    /* data */
+    char value_a;
+    int32_t value_b;
+} value_pair_t;
+
+typedef struct
+{
+    /* data */
+    value_pair_t *values;
+    size_t num_values;
+} records_t;
 
 
 /* DECLARATIONS */
 
+value_pair_t *create_pairs(const size_t num_pairs);
+
+void free_pairs(value_pair_t *pairs, const size_t num_pairs);
+
+records_t *create_records();
+
+void free_records(records_t *records);
 
 #endif /* RECORD_H */
